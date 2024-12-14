29cf05a49cdd8ffa33fd8f9bd80b267e5ffbd83e
Teatime <tealeafdrink@proton.me>
2024-12-14 04:45:26 +0100

    added main

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: egache <egache@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 14:14:11 by egache            #+#    #+#             */
-/*   Updated: 2024/12/03 05:17:40 by egache           ###   ########.fr       */
+/*   Updated: 2024/12/14 04:44:37 by egache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,9 @@ char	*get_next_line(int fd)
 	str[0] = '\0';
 	while (fd_read > 0 && ft_checkbl(buffer) == 0)
 	{
+		//printf("fd_read before : %d\n", fd_read);
 		fd_read = read(fd, buffer, BUFFER_SIZE);
+		//printf("fd_read after : %d\n", fd_read);
 		str = ft_strjoin(str, buffer);
 	}
 	return (str);
@@ -44,9 +46,16 @@ int	main(void)
 	char	*result;
 
 	fd = open("test.txt", O_RDONLY);
+	// fd = 0;
 	// printf("%s", get_next_line(fd));
-	result = get_next_line(fd);
-	printf("result : %s\n", result);
-	free(result);
+	// result = get_next_line(fd);
+	// printf("result : %s\n", result);
+	while ((result = get_next_line(fd)) != NULL)
+	{
+		printf("%s", result);
+		free(result);
+	}
+	close(fd);
+	//free(result);
 	return (0);
 }
